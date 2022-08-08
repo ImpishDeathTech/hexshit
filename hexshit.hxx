437423743a38f7f6dd96b5edceb7a5aeb3ed3351
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-08-08 20:07:06 +0000

    Add comments

--- hexshit.hxx
+++ hexshit.hxx
@@ -1,14 +1,32 @@
+/*
+* hexshit.cxx
+*
+* BSD 3-Clause License
+*
+* Copyright (c) 2022, Christopher Stephen Rafuse
+* All rights reserved.
+*/
+
 #ifndef HEXSHIT_HXX
 #define HEXSHIT_HXX
 
 #include <iostream>
 #include <cstdint>
 
+/*
+* data buffer for containing the original size and pointer to
+* the original program code, could be used with any pointer
+* in an active c++ program, like a homebrew game engine.
+*/
 typedef struct {
     void*  data { nullptr };
     size_t size { 0 };
 } DataBuffer;
 
+/*
+* Bytecode buffer used to iterate through the bytecode
+* DataBuffer's evil twin! AAAAAAAAAA
+*/
 typedef struct {
     uint8_t* data { nullptr };
     size_t   size { 0 };
